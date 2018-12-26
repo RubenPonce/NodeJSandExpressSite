f91e99b29d23cdd780072cd04a106586075e978f
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-12-26 01:38:56 -0800

    configured port to deploy to heroku

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use((err,req,res,next)=>{
   
 });
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!');
+app.listen(port, () => {
+    console.log(`The application is running on ${port}`);
 });
