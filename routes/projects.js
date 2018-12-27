720a56d9926170a34f9cebae5fcece7d7a2c1ae9
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-12-26 21:01:52 -0800

    fixed an error handling issue

--- routes/projects.js
+++ routes/projects.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/project:id', function(req, res) {
+router.get('/project:id', function(req, res, next) {
     const data = req.app.get('data');
     let {id} = req.params;
     id = parseInt(id)-1;
+    if(id>=0&&id<data.length){
     let projectInfo = data[id.toString()];
+
     res.render('project',{projectInfo});
+    }
+    else{
+        next();
+    }
 });
 module.exports = router; 
